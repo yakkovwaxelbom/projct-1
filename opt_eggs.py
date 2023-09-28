4031424efed558556facb560dee22a37d4af9942
yakkovwaxelbom <144027308+yakkovwaxelbom@users.noreply.github.com>
2023-09-28 15:30:30 +0300

    Add files via upload

--- opt_eggs.py
+++ opt_eggs.py
@@ -1,5 +1,9 @@
 import numpy as np
+import math
 
+
+# Initializes a matrix that will get the solutions to smaller problems with stopping conditions
+# f(n,1)=n f(n,0)=0 f(n,1)=1
 
 def table_opt(floors, eggs) -> any:
     table = [[0 for floor in range(floors + 1)] for egg in range(eggs + 1)]
@@ -11,13 +15,13 @@ def table_opt(floors, eggs) -> any:
     return table
 
 
+# Solve larger problems according to the formula f(n,e) = 1+min(1<=t<=n)max((t,e-1),(n-t,e)
 def compute_opt(floors, eggs, table) -> int:
-    #  [ 0  1  2  2  0  0  0  0  0  0  0]]
     for egg in range(2, eggs + 1):
         for floor in range(2, floors + 1):
             list_of_current_max = []
             for j in range(1, floor + 1):
-                list_of_current_max.append(max(table[egg -1][j - 1], table[egg][floor-j]))
+                list_of_current_max.append(max(table[egg - 1][j - 1], table[egg][floor - j]))
             table[egg][floor] = 1 + min(list_of_current_max)
     print(np.asmatrix(table))
     print(table[eggs][floors])
@@ -25,9 +29,32 @@ def compute_opt(floors, eggs, table) -> int:
     return table[eggs][floors]
 
 
-def manager_opt(floors, eggs):
+def manager_opt(floors, eggs) -> int:
     table = table_opt(floors, eggs)
     return compute_opt(floors, eggs, table)
 
 
-manager_opt(11, 2)
+# manager_opt(105, 2)
+
+
+def two_eggs(floor) -> int:
+    if floor == 1:
+        return 1
+    x = 1
+    count = 1
+    while x < floor:
+        x += count + 1
+        count += 1
+    return count
+
+
+# print(two_eggs(10))
+
+def two_eggs1(floor) -> float:
+    a = 1
+    b = 1
+    c = -2 * floor
+    return math.ceil((-b + (b ** 2 - 4 * a * c) ** 0.5) / (2 * a))
+
+
+print(two_eggs1(10))
